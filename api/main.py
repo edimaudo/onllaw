af1800e1f768bfeacf7b00c2599637b9b2788286
Edima <edimaudo@gmail.com>
2026-02-25 10:43:15 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, Form, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from .utils import extract_text_from_file
